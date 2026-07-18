9010510f512f018e88a29a88f8ec346bf5890198
PARANJOTHI-R <paranjothi8607@gmail.com>
2026-07-18 23:58:09 +0530

    Production rule change

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -90,8 +90,8 @@ export const login = async (req, res) => {
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+            secure: true,
+            sameSite:'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         });
 
@@ -108,8 +108,8 @@ export const logout = async (req, res) => {
     try {
         res.clearCookie('token', {
             httpOnly: true,
-            secure: process.env.NODE_ENV === 'production',
-            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict'
+            secure: true,
+            sameSite:'none'
         })
     }
     catch (error) {
